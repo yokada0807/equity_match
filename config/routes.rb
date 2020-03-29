973117dcba9f7a4b1e7b3494fec8a9f9f8d2f6a9
yoshihisa okada <yookada@winoo.co.jp>
2020-03-30 08:57:12 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root（トップページ「/」）に訪れた際にtopコントローラーのindexアクションを実行
   root 'top#index'
 
-  resources :users, only: %i(show)
+  resources :users, only: %i(index show)
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
